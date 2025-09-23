e3b253881d9b990edf69a3bdd3a9c19036edabd8
flammable121 <flammable121@gmail.com>
2025-09-23 09:07:59 +0300

    add variant style, mixins

--- components/UiButton.vue
+++ components/UiButton.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { NuxtLink } from "#components";
 
 export interface  UiButtonProps {
+  variant?: 'primary' | 'gold'
   to?: string;
   href?: string;
 }
 
 const props = withDefaults(defineProps<UiButtonProps>(), {
-
-})
+  variant: 'gold',
+});
 
 const componentType = computed(() => {
-  if (props.to) return 'NuxtLink';
+  if (props.to) return NuxtLink;
   if (props.href) return 'a';
   return 'button';
 })
@@ -22,41 +23,47 @@ const componentType = computed(() => {
       :is="componentType"
       :to="to"
       :href="href"
-      class="btn"
+      :class="['btn', `btn-${variant}`]"
   >
     <span v-if="$slots.default" class="btn__icon">
       <slot/>
     </span>
-    <span v-if="$slots.secondslot" class="btn__title">
-      <slot name="secondslot"/>
+    <span v-if="$slots.title" class="btn__title">
+      <slot name="title"/>
     </span>
   </component>
 </template>
 
 <style scoped lang="scss">
+@use "assets/style/mixins";
 .btn {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border: 1px solid black;
+  @include mixins.flex-center;
   gap: 10px;
+  min-width: 150px;
+  min-height: 40px;
   cursor: pointer;
-  width: 255px;
-  height: 50px;
 
-  @media (max-width: 870px) {
-    width: 160px;
-    height: 40px;
-    font-size: 12px;
-    padding: 5px;
-  }
 
   &:hover {
     box-shadow: 0 10px 15px map-get($colors, black);
   }
   &:active {
     box-shadow: none;
-    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  &-primary {
+    border: 1px solid black;
+    &:active {
+      background-color: rgba(0, 0, 0, 0.1);
+    }
+  }
+  &-gold {
+    border: none;
+    background-color: map-get($colors, gold);
+    color: map-get($colors, white);
+    &:active {
+      background-color: #CCA88A;
+    }
   }
 }
 </style>
