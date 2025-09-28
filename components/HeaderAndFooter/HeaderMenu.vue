737c895c3dd984f5d6f856f8948f8f1b701405c5
flammable121 <flammable121@gmail.com>
2025-09-28 19:04:22 +0300

    change header component

--- components/HeaderAndFooter/HeaderMenu.vue
+++ components/HeaderAndFooter/HeaderMenu.vue
@@ -9,28 +9,26 @@
       <div class="header__left">
         <Svgo-Burger class="header__burger"/>
         <nav class="header__nav">
-          <div class="header__nav-link typography--18">NEW</div>
-          <div class="header__nav-link typography--18">КАТАЛОГ</div>
-          <div class="header__nav-link typography--18">О НАС</div>
+          <NuxtLink to="/"  class="header__nav-link typography--18">NEW</NuxtLink>
+          <NuxtLink to="/" class="header__nav-link typography--18">КАТАЛОГ</NuxtLink>
+          <NuxtLink to="/" class="header__nav-link typography--18">О НАС</NuxtLink>
         </nav>
       </div>
 
       <div class="header__center">
-        <div class="header__logo">
-          <Svgo-Logo class="header__logo-icon"/>
-        </div>
+        <SvgoLogo class="header__center--logo"/>
       </div>
 
       <div class="header__right">
         <div class="header__selects">
-          <UiSelect class="header__select-selected"/>
-          <UiSelect class="header__select-selected"/>
+          <UiSelect class="header__selects--select"/>
+          <UiSelect class="header__selects--select"/>
         </div>
         <div class="header__menu">
-          <Svgo-Search class="header__menu-icon hidden"/>
-          <Svgo-Personalacc class="header__menu-icon hidden"/>
-          <Svgo-Favorites class="header__menu-icon"/>
-          <Svgo-Cart class="header__menu-icon"/>
+          <SvgoSearch class="header__menu-icon hidden"/>
+          <SvgoPersonalacc class="header__menu-icon hidden"/>
+          <SvgoFavorites class="header__menu-icon"/>
+          <SvgoCart class="header__menu-icon"/>
         </div>
       </div>
 
@@ -39,26 +37,25 @@
 </template>
 
 <style scoped lang="scss">
+@use "assets/variables/animation";
+@use "assets/style/breakpoints";
+@use "assets/style/mixins";
+
 .header {
   display: flex;
   width: 100%;
-  height: 85px;
 
   &__container {
     width: 100%;
     display: flex;
     justify-content: space-between;
-    padding: 0 150px;
-    transition: 0.2s;
-
-    @media (max-width: 1200px) {
-      padding: 0 30px;
-      transition: 0.2s;
-    }
+    margin: 0 auto;
+    padding: 24px 20px;
+    max-width: 1140px;
   }
 
   &__left {
-    width: 33.33%;
+    width: 33.333%;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -69,10 +66,10 @@
     height: 24px;
     color: map-get($colors, gold);
     cursor: pointer;
-
-    @media (max-width: 560px) {
-      width: 30px;
-      height: 20px;
+    @include animation.transition03;
+    @include breakpoints.media(mobile) {
+      width: 24px;
+      height: 24px;
     }
   }
 
@@ -80,66 +77,52 @@
     display: flex;
     gap: 25px;
 
-    @media (max-width: 870px) {
+    @include breakpoints.media(mobile) {
       display: none
     }
   }
 
-  &__nav-link {
-    cursor: pointer;
-  }
-
   &__center {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 33.33%;
-  }
-
-  &__logo {
-    &-icon {
-      width: 192px;
-      height: 40px;
-      color: map-get($colors, gold);
-      cursor: pointer;
-      transition: 0.3s;
-      @media (max-width: 560px) {
-        width: 79px;
-        height: 20px;
+    @include mixins.flex-center;
+    width: 33.333%;
+      &--logo {
+        width: 192px;
+        height: 40px;
+        color: map-get($colors, gold);
+        cursor: pointer;
+        @include animation.transition03
       }
     }
-  }
 
   &__right {
     display: flex;
     justify-content: flex-end;
     align-items: center;
-    width: 33.33%;
-  }
-
-  &__selects {
-    cursor: pointer;
+    width: 33.333%;
   }
 
   &__menu {
     display: flex;
     gap: 30px;
+
+    &-icon {
+      width: 25px;
+      height: 25px;
+      color: map-get($colors, gold);
+      cursor: pointer;
+      transition: 0.3s;
+
+      &:first-child {
+        color: black;
+      }
+
+      @include breakpoints.media(mobile) {
+        width: 20px;
+        height: 20px;
+      }
+    }
   }
 
-  &__menu-icon {
-    width: 25px;
-    height: 25px;
-    color: map-get($colors, gold);
-    cursor: pointer;
-    transition: 0.3s;
-    &:first-child {
-      color: black;
-    }
-    @media (max-width: 870px) {
-      width: 20px;
-      height: 20px;
-    }
-  }
   @media (max-width: 870px) {
     .hidden {
       transition: 0.3s;
